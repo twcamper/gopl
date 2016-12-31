fa5cec499196923cd4a53e9270ecf0b8d5f4f701
twcamper <twcamper@thoughtworks.com>
2016-12-30 19:55:32 -0600

    ex 1.8, default scheme; refactor

--- src/ch01/fetch.go
+++ src/ch01/fetch.go
@@ -4,23 +4,56 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
 )
 
 func main() {
-	for _, url := range os.Args[1:] {
-		response, err := http.Get(url)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-			os.Exit(1)
-		}
-		fmt.Printf("HTTP status: %v\n\n", response.Status)
-		size, err := io.Copy(os.Stdout, response.Body)
-		response.Body.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-			os.Exit(1)
-		}
-		fmt.Printf("\nRead %d bytes\n", size)
+	for _, arg := range os.Args[1:] {
+		url := toURL(arg)
+		response := getResponse(url.String())
+		reportMeta(response)
+		reportContent(response)
 	}
+}
+
+func toURL(s string) *url.URL {
+	_url, err := url.Parse(s)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch.toURL(): url unparsable '%s'\n%v\n", s, err)
+		os.Exit(1)
+	}
+	if _url.Scheme == "" {
+		_url.Scheme = "http"
+	}
+	return _url
+}
+
+func getResponse(url string) *http.Response {
+	r, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch.getResponse(): %v\n", err)
+		os.Exit(1)
+	}
+	return r
+}
+
+func reportMeta(r *http.Response) {
+	fmt.Printf("%s %s\n", r.Request.Method, r.Request.URL.String())
+	fmt.Println(r.Status)
+	fmt.Println("\nResponse Headers:")
+	for header, value := range r.Header {
+		fmt.Printf("\t%s: %s\n", header, value)
+	}
+	fmt.Println()
+}
+
+func reportContent(r *http.Response) {
+	size, err := io.Copy(os.Stdout, r.Body)
+	r.Body.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch.reportContent(): reading %s: %v\n", r.Request.URL.String(), err)
+		os.Exit(1)
+	}
+	fmt.Printf("\nRead %d bytes\n", size)
 }
