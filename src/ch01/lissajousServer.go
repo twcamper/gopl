d652189fb09c4c135e30007c0d109c574e6f67f2
twcamper <twcamper@thoughtworks.com>
2016-12-31 15:48:11 -0600

    extract image parameters to configuration object

--- src/ch01/lissajousServer.go
+++ src/ch01/lissajousServer.go
@@ -11,15 +11,6 @@ import (
 	"net/http"
 )
 
-var backgroundColor = color.Black
-var foregroundColor = color.RGBA{0x00, 0xa0, 0x00, 0xff}
-var palette = []color.Color{backgroundColor, foregroundColor}
-
-const (
-	backgroundIndex = 0
-	foregroundIndex = 1
-)
-
 func main() {
 	http.HandleFunc("/lissajous", handler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
@@ -27,33 +18,59 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	lissajous(w)
+	imageConfig := configure()
+	lissajous(w, imageConfig)
 }
-func lissajous(out io.Writer) {
+
+func lissajous(out io.Writer, cfg *ImageConfig) {
+
 	const (
-		cycles     = 5     // number of complete x oscillator revolutions
-		res        = 0.001 // angular resolution
-		size       = 350   // image canvas covers [-size..+size]
-		frameCount = 64    // animation frames
-		delay      = 8     // delay between frames in 10ms units
+		backgroundIndex = 0
+		foregroundIndex = 1
 	)
 
+	size := cfg.CanvasSize
+	backgroundColor := color.Black
+	foregroundColor := color.RGBA{0x00, 0xa0, 0x00, 0xff}
+	palette := []color.Color{backgroundColor, foregroundColor}
 	yFrequency := rand.Float64() * 3.0 // relative frequency of y oscillator
-	animation := gif.GIF{LoopCount: frameCount}
+	animation := gif.GIF{LoopCount: cfg.FrameCount}
 	phaseDifference := 0.0
 
-	for i := 0; i < frameCount; i++ {
+	for i := 0; i < cfg.FrameCount; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
-			x := math.Sin(t)
-			y := math.Sin(t*yFrequency + phaseDifference)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), foregroundIndex)
+		for t := 0.0; t < cfg.XOscillatorCycles*2*math.Pi; t += cfg.AngularResolution {
+			x := calculateColorIndexCoordinate(math.Sin(t), size)
+			y := calculateColorIndexCoordinate(math.Sin(t*yFrequency+phaseDifference), size)
+			img.SetColorIndex(x, y, foregroundIndex)
 		}
 
 		phaseDifference += 0.1
-		animation.Delay = append(animation.Delay, delay)
+		animation.Delay = append(animation.Delay, cfg.Delay)
 		animation.Image = append(animation.Image, img)
 	}
 	gif.EncodeAll(out, &animation) // NOTE: ignoring encoding errors
+}
+
+func calculateColorIndexCoordinate(rawCoordinate float64, size int) int {
+	offset := rawCoordinate*float64(size) + 0.5
+	return size + int(offset)
+}
+
+type ImageConfig struct {
+	XOscillatorCycles float64
+	AngularResolution float64
+	CanvasSize        int
+	FrameCount        int
+	Delay             int
+}
+
+func configure() *ImageConfig {
+	return &ImageConfig{
+		XOscillatorCycles: 5,
+		AngularResolution: 0.001,
+		CanvasSize:        350,
+		FrameCount:        64,
+		Delay:             8}
 }
