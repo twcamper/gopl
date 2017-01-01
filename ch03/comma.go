b2de388ecbd3300c727aa7a4c32691579f5508a4
twcamper <twcamper@thoughtworks.com>
2017-01-01 15:46:30 -0600

    ex 3.10 - comma format without recursion

---
+++ ch03/comma.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		fmt.Println(comma(arg))
+	}
+}
+
+func comma(s string) string {
+	var buffer bytes.Buffer
+	var to int = 3
+	if r := len(s) % 3; r > 0 {
+		to = r
+	}
+	for from := 0; to <= len(s); from, to = to, to+3 {
+		buffer.WriteString(s[from:to] + ",")
+	}
+	withCommas := buffer.String()
+	return withCommas[:len(withCommas)-1]
+}
