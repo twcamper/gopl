18ec47c82cbe063dd9058613fc248a6c6d38c9dd
twcamper <twcamper@thoughtworks.com>
2017-01-02 00:29:33 -0600

    simplification

--- ch04/adjacentDuplicates.go
+++ ch04/adjacentDuplicates.go
@@ -7,33 +7,18 @@ import (
 )
 
 func main() {
-	input := make([]string, len(os.Args[1:]))
-	copy(input, os.Args[1:])
-	fmt.Println(input)
-	elimnateAdjacentDuplicatesInPlace(input)
-	fmt.Println("In Place: %s", input)
-	fmt.Println("Via Copy: %s", elimnateAdjacentDuplicates(os.Args[1:]))
+	fmt.Println(os.Args[1:])
+	fmt.Println(eliminate(os.Args[1:]))
+	fmt.Println(os.Args[1:])
 }
 
-// does not shrink original slice, but replaces trailing elements with empty strings
-func elimnateAdjacentDuplicatesInPlace(strings []string) {
-	i := eliminate(strings)
-	blank := make([]string, len(strings)-i)
-	copy(strings[i:], blank)
-}
-
-func elimnateAdjacentDuplicates(strings []string) []string {
-	i := eliminate(strings)
-	return strings[:i]
-}
-
-func eliminate(strings []string) int {
-	newIndex := 1
+func eliminate(strings []string) []string {
+	i := 1
 	for previous, s := range strings[1:] {
 		if s != strings[previous] {
-			strings[newIndex] = s
-			newIndex++
+			strings[i] = s
+			i++
 		}
 	}
-	return newIndex
+	return strings[:i]
 }
