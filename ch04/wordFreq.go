7e6aaccd505c89a1fa710b8a0f49b2c99b5698be
twcamper <twcamper@thoughtworks.com>
2017-01-08 12:52:37 -0600

    Ex. 4.9 - word counter

---
+++ ch04/wordFreq.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var wordCounts map[string]int
+
+func init() {
+	wordCounts = make(map[string]int)
+}
+
+func main() {
+	count()
+	report()
+}
+
+func count() {
+	input := bufio.NewScanner(os.Stdin)
+	input.Split(bufio.ScanWords)
+	for input.Scan() {
+		wordCounts[strings.Trim(input.Text(), ",.'\";:!")]++
+	}
+	if err := input.Err(); err != nil {
+
+		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
+	}
+}
+
+func report() {
+	for word, count := range wordCounts {
+		fmt.Printf("%s: %d\n", word, count)
+	}
+}
