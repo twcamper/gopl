3b63c13313fdf5366b5bd54caaf54851f483e243
twcamper <twcamper@thoughtworks.com>
2017-01-08 23:37:18 -0600

    stub server returns full data set from local file

--- ch04/githubStubServer.go
+++ ch04/githubStubServer.go
@@ -1,33 +1,26 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-
-	"github.com/twcamper/gopl/ch04/github"
+	"os"
 )
 
+var stubFile string
+
 func main() {
+	stubFile = os.Args[1]
 	http.HandleFunc("/search/issues", handler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	data, err := json.Marshal(responseData())
+	data, err := ioutil.ReadFile(stubFile)
 	if err != nil {
-		log.Fatalf("JSON marshaling failed: %s", err)
+		log.Fatalf("%s: %v\n", os.Args[0], err)
 	}
-	fmt.Fprintf(w, "%s\n", data)
-}
 
-func responseData() *github.IssuesSearchResult {
-	eaigner := github.User{Login: "eaigner"}
-	gopherbot := github.User{Login: "gopherbot"}
-	item5680 := github.Issue{Number: 5680, User: &eaigner, Title: "encoding/json: set key converter on en/decoder"}
-	item6050 := github.Issue{Number: 6050, User: &gopherbot, Title: "encoding/json: provide tokenizer"}
-	item8658 := github.Issue{Number: 8658, User: &gopherbot, Title: "encoding/json: use bufio"}
-	items := []*github.Issue{&item5680, &item6050, &item8658}
-	return &github.IssuesSearchResult{len(items), items}
+	fmt.Fprintf(w, "%s\n", data)
 }
