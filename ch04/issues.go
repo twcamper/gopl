16fea5e711e2f3d377671c14fa881973470526c5
twcamper <twcamper@thoughtworks.com>
2017-01-10 04:24:34 -0600

    Ch 04 - issues, display createdAt

--- ch04/issues.go
+++ ch04/issues.go
@@ -10,6 +10,7 @@ import (
 )
 
 var serviceUrl string
+var result *github.IssuesSearchResult
 
 func init() {
 	flag.StringVar(&serviceUrl, "u", "", "alternate url")
@@ -18,14 +19,16 @@ func init() {
 
 func main() {
 	flag.Parse()
-	result, err := github.SearchIssues(url(), os.Args[1:])
+	get()
+	printHeader()
+	printRows()
+}
+
+func get() {
+	var err error
+	result, err = github.SearchIssues(url(), os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
-	}
-	fmt.Printf("%d issues:\n", result.TotalCount)
-	for _, item := range result.Items {
-		fmt.Printf("#%-5d %9.9s %.55s\n",
-			item.Number, item.User.Login, item.Title)
 	}
 }
 
@@ -34,4 +37,17 @@ func url() string {
 		return github.IssuesURL
 	}
 	return serviceUrl
+}
+
+func printRows() {
+	for _, item := range result.Items {
+		fmt.Printf("#%-5d %9.9s %s %.85s\n",
+			item.Number,
+			item.User.Login,
+			item.CreatedAt.Format("2006-01-02 15:04:05"),
+			item.Title)
+	}
+}
+func printHeader() {
+	fmt.Printf("%d issues:\n", result.TotalCount)
 }
